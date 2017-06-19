7b4154573824721d208f21c774a2667ccd0a14d8
Zachary Sunberg <sunbergzach@gmail.com>
2017-06-19 11:25:48 -0700

    required trusty so maybe latex will work now

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using POMDPGallery
 using Base.Test
 
-@test run_scripts(allow_failure=["LaserTag"])
+@test run_scripts()
 @test gen_readme("/tmp/test_README.md")
 
 @test readstring("/tmp/test_README.md") == readstring(Pkg.dir("POMDPGallery", "README.md"))
