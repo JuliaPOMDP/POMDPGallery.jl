1f8e9f84203f132ce406a9b2945b6b25c7124495
Zachary Sunberg <sunbergzach@gmail.com>
2017-06-19 20:28:09 -0700

    added sentence about copying and pasting

--- src/POMDPGallery.jl
+++ src/POMDPGallery.jl
@@ -7,7 +7,7 @@ function gen_readme(output=Pkg.dir("POMDPGallery", "README.md"))
     print(readme, """
         # POMDPGallery
 
-        A gallery of models written for [POMDPs.jl](https://github.com/JuliaPOMDP/POMDPs.jl) with visualizations.
+        A gallery of models written for [POMDPs.jl](https://github.com/JuliaPOMDP/POMDPs.jl) with visualizations. You should be able to copy and paste the code below each visualization to run it on your local machine.
 
         For instructions on how to add new models, see [INSTRUCTIONS.md](INSTRUCTIONS.md).
 
