33101aab36c876bff4e30fe388990007171b7a54
Zachary Sunberg <sunbergzach@gmail.com>
2017-06-21 10:20:20 -0700

    added badge to readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # POMDPGallery
 
+[![Build Status](https://travis-ci.org/JuliaPOMDP/POMDPGallery.jl.svg?branch=master)](https://travis-ci.org/JuliaPOMDP/POMDPGallery.jl)
+
 A gallery of models written for [POMDPs.jl](https://github.com/JuliaPOMDP/POMDPs.jl) with visualizations. You should be able to copy and paste the code below each visualization to run it on your local machine.
 
 For instructions on how to add new models, see [INSTRUCTIONS.md](INSTRUCTIONS.md).
--- src/POMDPGallery.jl
+++ src/POMDPGallery.jl
@@ -7,6 +7,8 @@ function gen_readme(output=Pkg.dir("POMDPGallery", "README.md"))
     print(readme, """
         # POMDPGallery
 
+        [![Build Status](https://travis-ci.org/JuliaPOMDP/POMDPGallery.jl.svg?branch=master)](https://travis-ci.org/JuliaPOMDP/POMDPGallery.jl)
+
         A gallery of models written for [POMDPs.jl](https://github.com/JuliaPOMDP/POMDPs.jl) with visualizations. You should be able to copy and paste the code below each visualization to run it on your local machine.
 
         For instructions on how to add new models, see [INSTRUCTIONS.md](INSTRUCTIONS.md).
