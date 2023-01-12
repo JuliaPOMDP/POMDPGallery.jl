740c222a3b899cf10c6dcf0ab4a06cad042de9ad
Felix Meng <felixmeng@DNa80a554.SUNet>
2023-01-11 23:20:25 -0800

    adding TikzPicture

--- problems/LaserTag/script.jl
+++ problems/LaserTag/script.jl
@@ -6,6 +6,7 @@ using ParticleFilters
 using Reel
 using POMDPTools
 using POMDPs
+using TikzPictures
 rng = MersenneTwister(7)
 
 m = gen_lasertag(rng=rng, robot_position_known=true)
@@ -14,22 +15,17 @@ filter = SIRParticleFilter(m, 10000, rng=rng)
 
 hr = HistoryRecorder(max_steps=1000, rng=rng)
 hist = simulate(hr, m, policy, filter)
-# num_steps = n_steps(hist)-1
 
 
 # @show makegif(m, policy, filter, filename="out.gif", rng=rng)
 
 frames = Frames(MIME("image/png"), fps=2)
-# for i in 1:num_steps
-#     s = state_hist(hist)[i+1]
-#     o = observation_hist(hist)[i]
-#     a = action_hist(hist)[i+1]
-#     b = belief_hist(hist)[i+1]
-#     r = reward_hist(hist)[i+1]
-#     push!(frames, LaserTagVis(p=m, a=a, s=s, r=r,o=o, b=b))
-# end
+
 for (s,b,a,o,r) in eachstep(hist, "s,b,a,o,r")
     push!(frames, LaserTagVis(m, s=s, a=a, o=o, b=b, r=r))
 end
+tp = TikzPicture(frames)
+save(SVG("out.gif"), tp)
 
-write("out.gif", frames)
+
+#write("out.gif", frames)
