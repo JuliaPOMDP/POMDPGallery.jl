e321bccda6db4b3ba0a27940b705e4ecf7caea31
Felix Meng <felixmeng@DNa80a554.SUNet>
2023-01-11 21:57:41 -0800

    update laser tag script

--- problems/LaserTag/script.jl
+++ problems/LaserTag/script.jl
@@ -14,18 +14,19 @@ filter = SIRParticleFilter(m, 10000, rng=rng)
 
 hr = HistoryRecorder(max_steps=1000, rng=rng)
 hist = simulate(hr, m, policy, filter)
+num_steps = n_steps(hist)-1
 
 
 # @show makegif(m, policy, filter, filename="out.gif", rng=rng)
 
 frames = Frames(MIME("image/png"), fps=2)
-for i in 1:n_steps(hist)-1
+for i in 1:num_steps
     s = state_hist(hist)[i+1]
     o = observation_hist(hist)[i]
     a = action_hist(hist)[i+1]
     b = belief_hist(hist)[i+1]
     r = reward_hist(hist)[i+1]
-    push!(frames, LaserTagVis(p, s=s, a=a, o=o, b=b, r=r))
+    push!(frames, LaserTagVis(m, s=s, a=a, o=o, b=b, r=r))
 end
 
 write("out.gif", frames)
