48fb84da3f07f8aaaa6122664422ee30e7b579a9
Felix Meng <felixmeng@DN0a229248.SUNet>
2023-01-11 18:10:57 -0800

    fixing Laser Tag

--- problems/LaserTag/script.jl
+++ problems/LaserTag/script.jl
@@ -20,14 +20,13 @@ hist = simulate(hr, m, policy, filter)
 # @show makegif(m, policy, filter, filename="out.gif", rng=rng)
 
 frames = Frames(MIME("image/png"), fps=2)
-for i in 1:ns
+for i in 1:n_steps(hist)-1
     s = state_hist(hist)[i+1]
     o = observation_hist(hist)[i]
     a = action_hist(hist)[i+1]
     b = belief_hist(hist)[i+1]
     r = reward_hist(hist)[i+1]
     push!(frames, LaserTagVis(p, s=s, a=a, o=o, b=b, r=r))
-    print(".")
 end
 
 write("out.gif", frames)
